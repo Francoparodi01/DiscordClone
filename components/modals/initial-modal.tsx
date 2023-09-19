dd1c328981d9e51a33b02c55464d1fba1e4fc6c8
Silvia Sudol <francoparodi2001@gmail.com>
2023-09-19 14:00:09 -0300

    Creando servidor

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import axios from "axios"
 import * as z from "zod"
 import {zodResolver} from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form";
+
 import {
     Dialog,
     DialogContent,
@@ -23,6 +25,7 @@ import { Input } from "@/components/ui/input"
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
 import { FileUpload } from "../file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -35,6 +38,9 @@ const formSchema = z.object({
 
 export const InitialModal = () =>{
     const [isMounted, setIsMounted] = useState(false);
+
+    const router =useRouter();
+
 
     useEffect(() =>{
         setIsMounted(true);
@@ -50,7 +56,15 @@ export const InitialModal = () =>{
 
     const isLoading = form.formState.isSubmitting;
     const onSubmit = async (values: z.infer<typeof formSchema>) =>{
-        console.log(values)
+        try {
+          await axios.post("/api/servers", values)
+          
+          form.reset();
+          router.refresh();
+          window.location.reload();
+        } catch (error) {
+            console.log(error)
+        }
     } 
 
 if (!isMounted) {
@@ -79,7 +93,11 @@ if (!isMounted) {
                                     render={({ field }) =>( 
                                         <FormItem>
                                             <FormControl>
-                                                <FileUpload/>
+                                                <FileUpload
+                                                  endpoint="serverImage"
+                                                  value={field.value}
+                                                  onChange={field.onChange}
+                                                />
                                             </FormControl>
                                         </FormItem>
                                     )}
