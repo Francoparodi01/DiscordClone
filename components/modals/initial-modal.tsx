6405fa293be76ddc1eb592a19f19a3b85758fceb
Silvia Sudol <francoparodi2001@gmail.com>
2023-09-19 12:29:12 -0300

    File hosting. Uploadthing docs

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -22,6 +22,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
+import { FileUpload } from "../file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -72,7 +73,17 @@ if (!isMounted) {
                     className="soace-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                todo: Image Upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) =>( 
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload/>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
                             <FormField
                                 control={form.control}
