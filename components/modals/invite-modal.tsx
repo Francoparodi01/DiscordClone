2267724bda3cbe03ae6e5fadc5f4190319940681
Francoparodi01 <francoparodi2001@gmail.com>
2023-10-04 13:19:45 -0300

    Invitaciones funcionales

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -85,7 +85,7 @@ export const InviteModal = () =>{
                         size="sm"
                         className="text-xs text-zinc-500 mt-4"
                     >
-                        Generate algo nuevo loko
+                        Generate a new link
                     <RefreshCw className="w-4 h-4 ml-2"/>
                     </Button>
                 </div>
