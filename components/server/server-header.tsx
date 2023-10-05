4810ffcddb3d263ecff76c2d0935292f2d396fc2
Francoparodi01 <francoparodi2001@gmail.com>
2023-10-05 15:21:03 -0300

    Configurando canales de audio y texto

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -83,6 +83,7 @@ export const ServerHeader = ({
                 {isModerator && (
                     <DropdownMenuItem
                         className=" px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("createChannel") }
                     >
                         Create channel
                         <PlusCircle className="h-4 w-4 ml-auto"/>
