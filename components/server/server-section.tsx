b0197124d8167a8ef1fe35452372adabcefddba0
Francoparodi01 <francoparodi2001@gmail.com>
2023-10-10 15:21:14 -0300

    channel types

--- components/server/server-section.tsx
+++ components/server/server-section.tsx
@@ -4,16 +4,17 @@ import { ServerWithMembersWithProfiles } from "@/types";
 
 import { ChannelType, memberRole } from "@prisma/client";
 import { ActionTooltip } from "../action-tooltip";
-import { Plus } from "lucide-react";
+import { Plus, Settings } from "lucide-react";
 import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerSectionProps {
     label: string;
     role?: memberRole;
     sectionType: "channels" | "members";
-    channelType: ChannelType;
+    channelType?: ChannelType;
     server?: ServerWithMembersWithProfiles;
-};
+  };
+
 export const ServerSection = ({
     label,
     role,
@@ -32,12 +33,23 @@ export const ServerSection = ({
                 <ActionTooltip label="Create channel" side="top">
                     <button 
                         className="text-zin-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
-                        onClick={()=> onOpen("createChannel")}
+                        onClick={()=> onOpen("createChannel", {channelType})}
                     >
                         <Plus className="h-4 w-4"/>
                     </button>
                 </ActionTooltip>
             )}
+            {role !== memberRole.GUEST && sectionType == "members" &&(
+                <ActionTooltip label="Manage members" side="top">
+                    <button 
+                        className="text-zin-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
+                        onClick={()=> onOpen("members", {server})}
+                    >
+                        <Settings className="h-4 w-4"/>
+                    </button>
+                </ActionTooltip>
+            )}
+
         </div>
     )
 }
