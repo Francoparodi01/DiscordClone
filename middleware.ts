23f8f661e6d04eb7259a1e7162d93174f0dc94e2
Francoparodi01 <francoparodi2001@gmail.com>
2024-03-25 20:22:54 -0300

    configurando llamadas

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
-
-// This middleware protects routes using Clerk authentication.
-// You can customize the configuration as needed.
-export default authMiddleware({});
-
-// Configuration for the middleware
+ 
+export default authMiddleware();
+ 
 export const config = {
-  // Matcher defines which routes should be protected.
-  // Here, it's set to protect all routes except those matching specific patterns.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
